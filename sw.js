a337c0e0c232d7dd0f17ecf9cea6622537475919
coralbarto-sys <coralbarto@gmail.com>
2026-06-06 17:10:38 +1000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE = 'budget-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/budget/'])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
